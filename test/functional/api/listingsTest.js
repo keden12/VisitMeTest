cefa85185002e2906f90a40e26dad2c77303aa2d
Kamil Bigos <keden123@hotmail.com>
2019-11-02 18:59:09 +0000

    API Testing Setup

---
+++ test/functional/api/listingsTest.js
@@ -0,0 +1,12 @@
+const chai = require("chai");
+const server = require("../../../bin/www");
+const expect = chai.expect;
+const request = require("supertest");
+const _ = require("lodash");
+
+let datastore = require("../../../models/listings");
+
+describe('Listings',  () => {
+//TODO
+
+});
