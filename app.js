f9d2886f12b620881eef145737f548e493c35335
Kamil Bigos <keden123@hotmail.com>
2019-11-05 14:15:51 +0000

    Watching scripts.

--- app.js
+++ app.js
@@ -1,66 +1,66 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors")
+var express = require("express")
+var path = require("path")
+var cookieParser = require("cookie-parser")
+var logger = require("morgan")
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index")
+var usersRouter = require("./routes/users")
 
 const listings = require("./routes/listings")
 
-var app = express();
+var app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs")
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger("dev"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, "public")))
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-
-app.get('/listings', listings.findListings);
-
-app.get('/listings/total', listings.findTotalListings);
-app.get('/listings/:category/total', listings.findTotalListingsByCategory);
-
-app.get('/listings/:id', listings.findByID);
-app.get('/listings/title/:title', listings.findByTitle);
-app.get('/listings/category/:category', listings.findByCategory);
+app.use("/", indexRouter)
+app.use("/users", usersRouter)
 
 
-app.put('/listings/:id/love', listings.incrementHeart);
+app.get("/listings", listings.findListings)
 
-app.put('/listings/:id/changetitle', listings.changeTitle);
-app.put('/listings/:id/changedesc', listings.changeDescription);
-app.put('/listings/:id/changefeatured', listings.changeFeatured);
+app.get("/listings/total", listings.findTotalListings)
+app.get("/listings/:category/total", listings.findTotalListingsByCategory)
 
-app.post('/listings',listings.addListing);
-app.post('/listings/search', listings.searchListings);
+app.get("/listings/:id", listings.findByID)
+app.get("/listings/title/:title", listings.findByTitle)
+app.get("/listings/category/:category", listings.findByCategory)
 
-app.delete('/listings/:id', listings.deleteListingByID);
-app.delete('/listings/title/:title', listings.deleteListingByTitle);
+
+app.put("/listings/:id/love", listings.incrementHeart)
+
+app.put("/listings/:id/changetitle", listings.changeTitle)
+app.put("/listings/:id/changedesc", listings.changeDescription)
+app.put("/listings/:id/changefeatured", listings.changeFeatured)
+
+app.post("/listings",listings.addListing)
+app.post("/listings/search", listings.searchListings)
+
+app.delete("/listings/:id", listings.deleteListingByID)
+app.delete("/listings/title/:title", listings.deleteListingByTitle)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
-});
+    next(createError(404))
+})
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res) {
+    // set locals, only providing error in development
+    res.locals.message = err.message
+    res.locals.error = req.app.get("env") === "development" ? err : {}
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+    // render the error page
+    res.status(err.status || 500)
+    res.render("error")
+})
 
-module.exports = app;
+module.exports = app
--- models/listings.js
+++ models/listings.js
@@ -1,27 +1,27 @@
-let mongoose = require('mongoose');
+let mongoose = require("mongoose")
 
 let ListingSchema = new mongoose.Schema({
-        title: String,
-        description: String,
-        category: String,
-        website:  {type: String, default: null},
-        mobile:  {type: String, default: null},
-        location: String,
-        featuredimage: String,
-        gallery:[{
-            type: String, default: null
-        }],
-        featured:{type: Boolean, default: false},
-        facebook: {type: String, default: ''},
-        instagram: {type: String, default: ''},
-        twitter: {type: String, default: ''},
-        hearts: {type: Number, default: 0},
-        reviews:[{
-            type: String, default: null
-        }]
-    },
-    {
-        collection: 'listings'
-    });
+    title: String,
+    description: String,
+    category: String,
+    website:  {type: String, default: null},
+    mobile:  {type: String, default: null},
+    location: String,
+    featuredimage: String,
+    gallery:[{
+        type: String, default: null
+    }],
+    featured:{type: Boolean, default: false},
+    facebook: {type: String, default: ""},
+    instagram: {type: String, default: ""},
+    twitter: {type: String, default: ""},
+    hearts: {type: Number, default: 0},
+    reviews:[{
+        type: String, default: null
+    }]
+},
+{
+    collection: "listings"
+})
 
-module.exports = mongoose.model('Listing', ListingSchema);
+module.exports = mongoose.model("Listing", ListingSchema)
